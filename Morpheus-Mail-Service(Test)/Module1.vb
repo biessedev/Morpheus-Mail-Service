4d32cc5908ddae97fec12d7bc2a0c094a10c5cc1
Paul Enășcuț <enstpaul@gmail.com>
2017-04-20 17:38:40 +0300

    adding a project for testing Morpheus-Mail windows service

---
+++ Morpheus-Mail-Service(Test)/Module1.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Configuration
+Imports Morpheus_Mail_Service_Test_
+
+Module Module1
+
+    Sub Main()
+        Morpheus_Console.Start()
+
+        'Dim builder As New Common.DbConnectionStringBuilder()
+        'builder.ConnectionString = ConfigurationManager.ConnectionStrings("Morpheus").ConnectionString
+        'Dim test As String = builder("host") + builder("database") + builder("username") + builder("password")
+        'Dim timer As New TimerECR(builder("host"), builder("database"), builder("username"), builder("password"))
+        'timer.TimerECR_Tick()
+    End Sub
+
+End Module
